3321212bc4914c80e51f24ce210df37833d5320e
ozermehmett <115498182+ozermehmett@users.noreply.github.com>
2023-03-15 00:30:24 +0200

    Telegram bot
    Bu botu kullanarak istediğiniz instagram kullanıcısının profil fotoğrafını edinebilirsiniz.

---
+++ README.txt
@@ -0,0 +1,10 @@
+botu kullanmak için öncelikle gerekli kütüphaneleri yükemeniz lazým aþaðýdaki satýrlarý cmd de çalýþtýrarak yükleyebilirsiniz
+
+pip3 install python-telegram-bot
+pip3 install instaloader
+
+
+þimdi botu çalýþýr hale getirmek için bot tokenýnýa ihtiyacýmýz var
+telegram üzerinden botfather dan botunuzu oluþturup size verdiði token ý 'main.py' içerisindeki 'TOKEN' deðþkenine atayýnýz
+
+botunuz kullanýma hazýr telegrama gidip  '/start'  komutuyla botunuzu test edebilirsiniz.
---
+++ telegramBot/main.py
@@ -0,0 +1,33 @@
+import telegram
+import instaloader
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = 'BOT TOKEN'   #botunuzun tokenini buraya yapıştırıyoruz
+
+bot = telegram.Bot(token=TOKEN)
+
+L = instaloader.Instaloader()
+
+def start(update, context):   #start komutuna verilecek cevap 
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Merhaba! Bu bot Instagram profil fotoğrafını göstermek için hazır.\nLütfen şu formatta yazınız: \n\n/profile username")
+
+def profile(update, context):   #profile komutu gelirse döndürülecek fonk.
+    username = context.args[0]
+    print(username)
+    
+    try:
+        profile = instaloader.Profile.from_username(L.context, username)
+        
+        profile_pic_url = profile.profile_pic_url
+        
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=profile_pic_url)
+        
+    except:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hata! Lütfen geçerli bir kullanıcı adı girin.")
+
+updater = Updater(TOKEN, use_context=True)
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('profile', profile))
+
+updater.start_polling()
+updater.idle()
